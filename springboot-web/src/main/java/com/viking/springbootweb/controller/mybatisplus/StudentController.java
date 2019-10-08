67d84cb1b2ec3ca6e5a7d109ed9cf8c3e4993585
viking <viking_yan@163.com>
2019-10-08 17:28:13 +0800

    提交合并

--- springboot-web/src/main/java/com/viking/springbootweb/controller/mybatisplus/StudentController.java
+++ springboot-web/src/main/java/com/viking/springbootweb/controller/mybatisplus/StudentController.java
@@ -1,24 +1,24 @@
-package com.viking.springbootweb.controller.mybatisplus;
-
-import com.viking.springbootmybatisplus.dao.StudentMapper;
-import com.viking.springbootmybatisplus.service.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * Created by Viking on 2019/9/17
- */
-@RestController
-@RequestMapping("/mp")
-public class StudentController {
-    @Autowired
-    private StudentService service;
-
-
-    @GetMapping("stu")
-    public Object getStudentById(long id){
-        return service.getStudentById(id);
-    }
-}
+//package com.viking.springbootweb.controller.mybatisplus;
+//
+//import com.viking.springbootmybatisplus.dao.StudentMapper;
+//import com.viking.springbootmybatisplus.service.StudentService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * Created by Viking on 2019/9/17
+// */
+//@RestController
+//@RequestMapping("/mp")
+//public class StudentController {
+//    @Autowired
+//    private StudentService service;
+//
+//
+//    @GetMapping("stu")
+//    public Object getStudentById(long id){
+//        return service.getStudentById(id);
+//    }
+//}
---
+++ springboot-web/src/test/java/com/viking/springbootweb/mytest/MyTest.java
@@ -0,0 +1,43 @@
+package com.viking.springbootweb.mytest;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+/**
+ * Created by yanshuai on 2019/10/8
+ * 测试将一张图片放到另一张图片上
+ */
+public class MyTest {
+    /**
+     *param qrcodePath : 最后图片保存路劲
+     */
+    public static void overlapImage() {
+        try {
+            String qrcodePath = "E:\\FileTest\\createdPic.jpg";
+            BufferedImage big = new BufferedImage(1080, 1920, BufferedImage.TYPE_INT_RGB);
+            Graphics2D gd = big.createGraphics();
+            //gd.setBackground(Color.white);
+            gd.setColor(Color.white);
+            gd.dispose();
+            //BufferedImage big = ImageIO.read(new File(screenPath));
+            //BufferedImage small = ImageIO.read(new File(qrcodePath));
+            BufferedImage small = new BufferedImage(540, 540,BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g = big.createGraphics();
+            int x = (big.getWidth() - small.getWidth()) / 2;
+            int y = 200 ;
+            g.drawImage(small, x, y, small.getWidth(), small.getHeight(), null);
+            g.dispose();
+            ImageIO.write(big, "jpg", new File(qrcodePath));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        overlapImage();
+        System.out.println("done");
+    }
+
+}
